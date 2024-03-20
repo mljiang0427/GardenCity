40900a5d7aeed473b3e55d83ddd72b6d43e8db5d
MEILING JIANG <mljiang0427@gmail.com>
2024-03-20 14:44:38 -0400

    update product api

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,9 +2,17 @@ package org.example;
 
 import io.muserver.*;
 import io.muserver.rest.RestHandlerBuilder;
+import org.example.db.DatabaseFactory;
 import org.example.handler.ProductHandler;
 import org.example.handler.UserHandler;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import static io.muserver.ContextHandlerBuilder.context;
 import static io.muserver.MuServerBuilder.httpServer;
@@ -21,10 +29,32 @@ public class App
                             .addHandler(RestHandlerBuilder.restHandler(new ProductHandler())))
                     .start();
             System.out.println("Service is running on..." + server.uri());
+//            insertProducts();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
+    }
+    public static void insertProducts() {
+        Path directory = Paths.get(App.class.getClassLoader().getResource("imgs").getFile());
+        if (!Files.exists(directory) || !Files.isDirectory(directory)) {
+            System.out.println("Directory does not exist or is not a directory.");
+            return;
+        }
+        try {
+            Files.list(directory).forEach(path -> {
+                try(Connection connection = DatabaseFactory.getInstance().getConnection();
+                    PreparedStatement ps = connection.prepareStatement("INSERT into products (id, name, price, image) values (uuid(), ?, ?, ?);")) {
+                    ps.setString(1, path.getFileName().toString().substring(0, path.getFileName().toString().indexOf(".")));
+                    ps.setDouble(2, (Math.random() + 3) + (Math.random() * 10 + Math.random()));
+                    ps.setString(3, "http://localhost:8080/garden_city/product/img/" + path.getFileName());
+                    ps.execute();
+                    System.out.println("successfully added product " + path.getFileName().toString().substring(0, path.getFileName().toString().indexOf(".")));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/handler/ProductHandler.java
+++ src/main/java/org/example/handler/ProductHandler.java
@@ -1,15 +1,14 @@
 package org.example.handler;
 
-import com.google.gson.Gson;
+import io.muserver.UploadedFile;
 import org.example.db.DatabaseFactory;
-import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.InputStream;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,19 +18,35 @@ import java.util.UUID;
 public class ProductHandler {
 
     @GET
+    @Path("/img/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getImg(@PathParam("name") String imgName) {
+        try(InputStream inputStream =  ProductHandler.class.getClassLoader().getResourceAsStream("imgs/" + imgName)) {
+            return Response.status(Response.Status.OK)
+                    .entity(inputStream.readAllBytes())
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Service error")
+                    .build();
+        }
+    }
+
+    @GET
     @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getProducts() {
         try (Connection connection = DatabaseFactory.getInstance().getConnection();
-             PreparedStatement ps = connection.prepareStatement("SELECT products.id, products.price, products.name, p_images.img FROM products JOIN p_images on products.id = p_images.productId");
+             PreparedStatement ps = connection.prepareStatement("SELECT id, name, price, image FROM products;");
              ResultSet rs = ps.executeQuery()) {
              JSONArray result = new JSONArray();
              while(rs.next()) {
                  JSONObject o = new JSONObject();
                  o.put("id", rs.getString("id"));
-                 o.put("price", rs.getString("price"));
                  o.put("name", rs.getString("name"));
-                 o.put("image", rs.getBytes("img"));
+                 o.put("price", rs.getString("price"));
+                 o.put("image", rs.getString("image"));
                  result.put(o);
              }
             return Response.status(Response.Status.OK)
@@ -49,23 +64,19 @@ public class ProductHandler {
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
     public Response addProduct(
-            @FormDataParam("image") InputStream image,
-            @QueryParam("name") String name,
-            @QueryParam("price") String price) {
+            @FormParam("name") String name,
+            @FormParam("price") String price,
+            @FormParam("image") UploadedFile image) {
         try {
             UUID uuid = UUID.randomUUID();
             String uuidString = uuid.toString();
             try (Connection connection = DatabaseFactory.getInstance().getConnection();
-                 PreparedStatement ps = connection.prepareStatement("INSERT INTO products(id, name, price) values (?, ?, ?);");) {
+                 PreparedStatement ps = connection.prepareStatement("INSERT INTO products(id, name, price, img) values (?, ?, ?, ?);");) {
                 ps.setString(1, uuidString);
                 ps.setString(2, name);
                 ps.setDouble(3, Double.parseDouble(price));
+                ps.setBytes(4, image.asBytes());
                 ps.execute();
-                try(PreparedStatement ps1 = connection.prepareStatement("INSERT INTO p_images(id, productId, img) values(uuid(), ?, ?);");) {
-                    ps1.setString(1, uuidString);
-                    ps1.setBlob(2, image);
-                    ps1.execute();
-                }
                 return Response.status(Response.Status.OK)
                         .entity("Successfully added product with id: " + uuidString)
                         .build();
@@ -78,10 +89,4 @@ public class ProductHandler {
         }
     }
 
-//    @GET
-//    @Path("/getImg/{}") //path param
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Response getProductImg() {
-//
-//    }
 }
